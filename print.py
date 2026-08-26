110bfdf372f0bf9a8e5248d9d54237c034500152
LucasAndre0719 <glucasbr370@gmail.com>
2026-08-26 15:24:02 -0300

    feat: Atualização dos codigos da pasta projeto

--- print.py
+++ print.py
@@ -1,9 +1,9 @@
 # Print simple de um hello world!
-print("Hello, World!")
+#print("Hello, World!")
 
 
 #basico de função
-def saudacao(nome):
+"""def saudacao(nome):
     print(f"Olá, {nome}!")
 
 saudacao("Alice")
@@ -15,5 +15,16 @@ saudacao("Charlie")
 def soma(num_1, num_2):
     return num_1 + num_2
 
-print(soma(3, 5))
+print(soma(3, 5))"""
+
+#
+""""usuarios = [{"nome": "Alice", "Idade": 20},
+            {"nome": "Jorge", "Idade": 25},
+            {"nome": "Lucas", "Idade": 22}]
+
+
+for usuario in usuarios:
+
+    print(f"Nome:{usuario['nome']}, Idade:{usuario['Idade']}")"""
+
 
